b5ffd202466bf55749777085f09819e935bcb63b
Aleem Rahil <aleem.rahil@outlook.com>
2024-05-18 15:54:45 -0400

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -9,7 +9,7 @@ WITH
     FROM
       aleemrahil84520.actors_history_scd
     WHERE
-      current_year = 1979
+      current_year = 2021
   ),
   -- CTE to get actor data for the next year 
   current_year_scd AS (
@@ -18,7 +18,7 @@ WITH
     FROM
       aleemrahil84520.actors
     WHERE
-      current_year = 1980
+      current_year = 2022
   ),
   -- CTE to select data and combine them to insert into the actor history table
   combined AS (
@@ -39,7 +39,7 @@ WITH
         OR ls.is_active <> cs.is_active THEN 1
         ELSE NULL
       END AS did_change,
-      1979 AS current_year
+      2022 AS current_year
     FROM
       last_year_scd ls
       FULL OUTER JOIN current_year_scd cs ON ls.actor = cs.actor
