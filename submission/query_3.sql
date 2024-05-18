12c5ac2ba1394ae4eca76e8ca47167aeb96a4ad9
Aleem Rahil <aleem.rahil@outlook.com>
2024-05-18 15:19:16 -0400

    Update query_3.sql

---
+++ submission/query_3.sql
@@ -0,0 +1,11 @@
+CREATE TABLE aleemrahil84520.actors_history_scd (
+    actor VARCHAR,
+    quality_class VARCHAR,
+    is_active BOOLEAN,
+    start_date DATE,
+    end_date DATE,
+    current_year INTEGER
+)
+with (
+format = 'parquet',
+partitioning = array['current_year'])
