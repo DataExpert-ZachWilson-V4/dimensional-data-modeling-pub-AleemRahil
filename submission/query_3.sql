53815eda428bd3b9e2c594b37284839ae6118e6b
Aleem Rahil <aleem.rahil@outlook.com>
2024-05-18 15:28:14 -0400

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,4 +1,4 @@
-CREATE TABLE aleemrahil84520.actors_history_scd (
+CREATE OR REPLACE TABLE aleemrahil84520.actors_history_scd (
     actor VARCHAR,
     quality_class VARCHAR,
     is_active BOOLEAN,
