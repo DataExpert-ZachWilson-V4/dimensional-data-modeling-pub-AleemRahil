4f7006cb618b0445af8641d3a625926d756cd0ff
Aleem Rahil <aleem.rahil@outlook.com>
2024-05-18 15:29:30 -0400

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,12 +1,12 @@
-CREATE TABLE aleemrahil84520.actors (
+CREATE OR REPLACE TABLE aleemrahil84520.actors (
     actor VARCHAR,
     actor_id VARCHAR,
-    films ARRAY<STRUCT<
-        film: VARCHAR,
-        votes: INTEGER,
-        rating: INTEGER,
-        film_id: VARCHAR
-    >>,
+    films ARRAY(ROW(
+        film VARCHAR,
+        votes INTEGER,
+        rating INTEGER,
+        film_id VARCHAR
+    )),
     quality_class VARCHAR,
     is_active BOOLEAN,
     current_year INTEGER
